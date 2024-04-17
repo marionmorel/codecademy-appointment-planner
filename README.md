18c60ece2bedf6365ed1e9e5a04b8dbbf7737efd
ma-mrl <marion.mrl@live.fr>
2024-04-17 17:00:31 +0200

    implemented contactPage component

--- README.md
+++ README.md
@@ -8,3 +8,28 @@ Instead of a step-by-step tutorial, this project contains a series of open-ended
 
 In this project, you will use functional React components to create an app that manages contacts and appointments. The app consists of two pages: one to view and add contacts and one to view and add appointments.
 You will work with stateful and stateless functional React components using hooks. The requirements section will walk through implementing the app from the topmost component down. If you would like to implement it in a different order, feel free to do what is comfortable for you.
+
+### Requirements 
+
+### <code>App</code>
+
+- Keep track of the contacts and appointments data, each being an array of objects
+- Define a callback function that, given a name, phone number, and email, adds a new contact object with that data to the array of contacts
+- Define a callback function that, given a name, contact, date, and time, adds a new appointment object with that data to the array of appointments
+- Pass the array of contacts and the appropriate callback function as props to the <code>ContactsPage</code> component
+- Pass the appointments array, contacts array, and the add appointment function as props to the <code>AppointmentsPage</code> component
+
+### <code>ContactsPage</code>
+
+- Receive two props:
+    - The current list of contacts
+    - A callback function for adding a new contact
+- Keep track of three local state values: the current name, phone, and email entered into the form
+- Check for duplicates whenever the name in the form changes and indicate the name is a duplicate
+- Only add a new contact on form submission if it does not duplicate an existing contact’s name
+- A successful submission should clear the form
+- In the Add Contact section, render a <code>ContactForm</code> with the following passed via <code>props</code>:
+    - local state variables
+    - local state variable setter functions
+    - <code>handleSubmit</code> callback function
+- In the Contacts section, render a <code>TileList</code> with the contact array passed via <code>props</code>
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,33 +3,63 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
+export const ContactsPage = ({ contacts, addContacts }) => {
+  /* Define state variables for contact info and duplicate check */
+  const [name, setName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [duplicate, setDuplicate] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
-    */
+    /* Add contact info and clear data if the contact name is not a duplicate */
+    if(!duplicate) {
+      addContacts(name, phoneNumber, email);
+      setName('');
+      setPhoneNumber('');
+      setEmail('');
+    }
   };
 
-  /*
-  Using hooks, check for contact name in the 
-  contacts array variable in props
-  */
+  /* Using hooks, check for contact name in the contacts array variable in props */
+  useEffect(() => {
+    const nameIsDuplicate = () => {
+      const found = contacts.find((contact) => contact.name === name);
+      if (found !== undefined) {
+        return true;
+      } return false 
+    };
+
+    if (nameIsDuplicate()) {
+      setDuplicate(true)
+    } else {
+      setDuplicate(false)
+    }
+  }, [name, contacts, duplicate]);
+
 
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact
+        {duplicate ? " - Name Already Exists" : ""}
+        </h2>
+        <ContactForm 
+          name={name}
+          setName={setName}
+          phoneNumber={phoneNumber}
+          setPhoneNumber={setPhoneNumber}
+          email={email}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
+        /> 
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList 
+          tiles={contacts}
+        />
       </section>
     </div>
   );
